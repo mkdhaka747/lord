62d7e8c3fd4e054f6527da7c8ecbb8f1bf6f646e
shiva <82491526+mkdhaka747@users.noreply.github.com>
2022-02-17 17:04:02 +0530

    Update helpers.py

--- main/plugins/helpers.py
+++ main/plugins/helpers.py
@@ -1,4 +1,4 @@
-#Github.com/Vasusen-code
+# Github.com/mkdhaka747
 
 from pyrogram.errors import FloodWait, InviteHashInvalid, InviteHashExpired, UserAlreadyParticipant
 from telethon import errors, events
